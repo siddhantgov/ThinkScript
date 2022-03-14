e175308234be9c9d13727b25ff27a003c0159995
siddhantgov <85798439+siddhantgov@users.noreply.github.com>
2022-03-13 22:01:50 -0400

    Add files via upload
    Custom indicator where the linear regression curve of the price is calculated and then the distance between the price and the calculated linear regression curve is plotted.

---
+++ DisToLinearCurveSTUDY.ts
@@ -0,0 +1,18 @@
+
+input displace = 0;
+input length = 60;
+input price = close;
+def LinReg = LinearRegCurve(displace, length, price);
+def LinDis = ((price - LinReg) / (price));
+plot LinDi = LinDis;
+
+#Extras
+input LinType = AverageType.EXPONENTIAL;
+#def LINMA = MovingAverage(LinType, LinDis, 14);
+#plot LINMA1 = LINMA;
+#AddLabel(yes, LinDis + "%");
+#def zeroline =0;
+#plot data = zeroline;
+
+    
+
